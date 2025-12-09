81c759838c2c2c21f14b245a691b1783aa026ebf
Kraus-Mara <krausantoi@cy-tech.fr>
2025-12-09 15:46:38 +0100

    no need to declare system, its in the flake

--- dentrix.nix
+++ dentrix.nix
@@ -2,7 +2,6 @@
 { inputs, ...} : {
   nixosConfigurations = {
     dentrix = {
-      system = "x86_64-linux";
       modules = [
         ./modules/hardware/hardware-configuration.nix
 	./modules/core/default.nix
