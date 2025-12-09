b193e8ec3224ab3360a3b98707b151ce9f95e43c
Kraus-Mara <krausantoi@cy-tech.fr>
2025-12-09 15:37:38 +0100

    flake.nix skeleton

--- flake.nix
+++ flake.nix
@@ -1,20 +1,18 @@
+# flake skeleton
 {
-  description = "Dendritic pattern skeleton";
-
+  description = "";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github.com:hercules-ci/flake-parts";
+    nixpkgs.url = "github:nixos/nixpkgs/pkgs-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs = inputs@{flake-parts, ...}:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64_linux" ];
 
-      imports = [ # Simply put your hardware-configuration.nix in the parts/ folder then complete the config.nix skeleton
-        ./parts/config.nix
-	./parts/system.nix # Here's the dotNix file that will import hardware-configuration.nix
+      imports = [
+        ./dentrix.nix
       ];
-
-      flake = {};
     };
 }
