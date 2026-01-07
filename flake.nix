1038858d384460c5f0bcfbc9dcf9705cdab14785
Kraus-Mara <krausantoi@cy-tech.fr>
2026-01-07 15:12:00 +0100

    added : more inputs !

--- flake.nix
+++ flake.nix
@@ -1,8 +1,18 @@
 # Taken from vic : https://github.com/vic/dendrix/blob/main/flake.nix
 {
   outputs = inputs: import ./. inputs;
-  inputs.import-tree.url = "github:vic/import-tree";
-  inputs.nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
-  # I think the explaination about nixpkgs-lib is that we can load lib without loading pkgs. 
-  # Loading nixos-unstable / stable-XX would be too heavy and slow compared to this.
+  inputs = {
+    import-tree.url = "github:vic/import-tree";
+    nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable ";
+
+    hyprland.url = "github:hyprwm/Hyprland";
+    hypr-contrib.url = "github:hyprwm/contrib";
+    hyprpicker.url = "github:hyprwm/hyprpicker";
+
+    home-manager.url = "github:nix-community/home-manager";
+
+    spicetify-nix.url = "github:gerg-l/spicetify-nix";
+  };
 }
+
