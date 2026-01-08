77d972fa54c5fa545945a653a2a84b6c077d7f35
Kraus-Mara <krausantoi@cy-tech.fr>
2026-01-08 14:37:32 +0100

    modified : flake.nix

--- flake.nix
+++ flake.nix
@@ -1,18 +1,23 @@
-# Taken from vic : https://github.com/vic/dendrix/blob/main/flake.nix
+# Inspired from vic : https://github.com/vic/dendrix/blob/main/flake.nix
 {
-  outputs = inputs: import ./. inputs;
+  outputs = inputs:
+    let
+      layers = import ./. inputs;
+    in
+    {
+      imports = builtins.attrValues layers;
+      systems = [ "x86_64-linux" ];
+    };
+
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
     nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable ";
-
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     hyprland.url = "github:hyprwm/Hyprland";
     hypr-contrib.url = "github:hyprwm/contrib";
     hyprpicker.url = "github:hyprwm/hyprpicker";
-
     home-manager.url = "github:nix-community/home-manager";
-
     spicetify-nix.url = "github:gerg-l/spicetify-nix";
   };
 }
-
